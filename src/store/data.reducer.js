1709d0f8952aff42b2288a4a9f275b5a33ec14f1
Nghia Mai <mvtnghia@gmaiil.com>
2017-05-27 15:30:50 +0700

    Handle Object Data

--- src/store/data.reducer.js
+++ src/store/data.reducer.js
@@ -1,9 +1,10 @@
 import defaultState from './defaultState';
+import handleObjectData from './data.reducer.object';
 
 export default function dataReducer(state = defaultState, action) {
     switch (action.type) {
         case 'SET_DATA':
-            return null;
+            return handleObjectData({ state, ...action });
 
         default:
             return state;
--- src/store/data.reducer.object.js
+++ src/store/data.reducer.object.js
@@ -3,27 +3,28 @@ import jsonPatch from 'jsonpatch';
 /**
  * Apply JSON Patch for multiple places on state
  * @param state
+ * @param type
  * @param rootPath
- * @param patch
+ * @param path
  * @returns new state (immutable)
  */
-function applyPatch({ state, rootPath, patch }) {
+
+export default function handleObjectData({ state, type, rootPath, path }) {
     try {
-        const patchObj = rootPath ? patch.map(item => ({ item, path: `${rootPath}/${item.path}` })) : patch;
-        return jsonPatch.apply_patch(state, patchObj);
+        if (!rootPath && !path) throw new Error('rootPath & path cannot be null');
+        if (rootPath.charAt(0) !== '/'
+            && path.charAt(0) !== '/'
+            && rootPath.charAt(rootPath.length - 1) === '/'
+            && path.charAt(path.length - 1) === '/') {
+            throw new Error('rootPath & path are invalid');
+        }
+        const op = type.substring(type.indexOf('_') + 1);
+        const patchData = rootPath ? (path || []).map(i => ({ ...i, op, path: `${rootPath}${i.path}` })) : path;
+
+        return jsonPatch.apply_patch(state, patchData);
     } catch (e) {
         console.warn('Error when trying to patch state');
         console.warn(e);
         return state;
     }
-}
-
-
-export default function handleObjectData({ state, patch, values = [] }) {
-    return {
-        OBJECT_ADD: applyPatch({ state, action: 'add', patch, values }),
-        OBJECT_REMOVE: 1,
-        OBJECT_UPDATE: 1,
-        OBJECT_DELETE: 1,
-    };
 }
