4a2ee8b482e57c7b9032c363375ff65ed8140db0
Nghia Mai <mvtnghia@gmaiil.com>
2017-06-07 18:41:56 +0700

    Add a common reducer Add createActionCreator

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import middleware from '../middleware';
 import rootReducer from './index.reducer';
 import rootSaga from './index.saga';
+import defaultState from './defaultState';
 
 export default function configureStore({ history }) {
     const sagaMiddleware = createSagaMiddleware();
@@ -12,7 +13,7 @@ export default function configureStore({ history }) {
     // eslint-disable-next-line no-underscore-dangle
     const reduxDEC = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
     const composeEnhancers = (process.env.NODE_ENV !== 'production' && reduxDEC) ? reduxDEC({}) : compose;
-    const store = createStore(rootReducer, undefined, composeEnhancers(
+    const store = createStore(rootReducer, defaultState, composeEnhancers(
         applyMiddleware(...middleware, routeMiddleWare, sagaMiddleware),
     ));
     sagaMiddleware.run(rootSaga);
