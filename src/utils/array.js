793e30e13fdbce05fd5906547c956037d2bc5cf9
Nghia Mai <mvtnghia@gmaiil.com>
2018-03-10 16:06:46 +0700

    update immutable

--- src/utils/array.js
+++ src/utils/array.js
@@ -3,10 +3,20 @@
  * @param arr
  * @returns {*}
  */
-function arrayToObject(arr) {
+export function arrayToObject(arr) {
 	return arr.reduce((result, i) => ({ ...result, [i]: i }), {});
 }
 
-export {
-	arrayToObject,
-};
+export function groupArrayByField(array, fieldName, original = false) {
+	const result = array.reduce((acc, item) => {
+		const existingValue = acc.get(item[fieldName]);
+		if (existingValue) {
+			acc.set(item[fieldName], existingValue.concat([item]));
+		} else {
+			acc.set(item[fieldName], [item]);
+		}
+		return acc;
+	}, new Map());
+
+	return original ? result : Array.from(result);
+}
