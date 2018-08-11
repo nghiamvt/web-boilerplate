2068e0e8329c177b10c8566bd04d8af49a1fbf32
Nghia Mai <mvtnghia@gmaiil.com>
2018-08-11 19:41:36 +0700

    upgrade dependencies package

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 
@@ -17,11 +18,13 @@ export default function Routes() {
 		<Provider store={store}>
 			<ConnectedRouter history={history}>
 				<Master>
-					<Switch>
-						<Route exact path="/" component={App} />
-						<Route path="/todo" component={Todo} />
-						<Route component={() => (<div>404 Not found</div>)} />
-					</Switch>
+					<BrowserRouter>
+						<Switch>
+							<Route exact path="/" component={App} />
+							<Route path="/todo" component={Todo} />
+							<Route component={() => (<div>404 Not found</div>)} />
+						</Switch>
+					</BrowserRouter>
 				</Master>
 			</ConnectedRouter>
 		</Provider>
