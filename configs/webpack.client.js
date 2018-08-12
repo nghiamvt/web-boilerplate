bb18cbe838c6f72a26ca86c430cdd73eabeb6174
Nghia Mai <mvtnghia@gmaiil.com>
2018-08-12 17:12:06 +0700

    init

--- configs/webpack.client.js
+++ configs/webpack.client.js
@@ -37,75 +37,94 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			// "file" loader makes sure those assets end up in the `build` folder.
-			// When you `import` an asset, you get its filename.
 			{
-				exclude: [/\.html$/, /\.(js|jsx)$/, /\.styl$/, /\.json$/, /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-				loader: 'file-loader',
-				options: {
-					name: 'static/media/[name].[hash:8].[ext]',
-				},
-			},
-			// "url" loader works just like "file" loader but it also embeds
-			// assets smaller than specified size as data URLs to avoid requests.
-			{
-				test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-				loader: 'url-loader',
-				options: {
-					limit: 10000,
-					name: 'static/media/[name].[hash:8].[ext]',
-				},
-			},
-			{
-				test: /\.js?$/,
-				include: paths.appSrc,
-				loaders: 'babel-loader',
-				options: {
-					cacheDirectory: true,
-				},
-			},
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [
-						{
-							loader: 'css-loader',
-							options: {
-								importLoaders: 2,
-								minimize: isProduction,
-								sourceMap: !isProduction,
-							},
+				// only the first matching Rule is used when the Rule matches.
+				oneOf: [
+					// "url" loader works like "file" loader except that it embeds assets
+					// smaller than specified limit in bytes as data URLs to avoid requests.
+					{
+						test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+						loader: 'url-loader',
+						options: {
+							limit: 10000,
+							name: 'images/[name].[hash:8].[ext]',
 						},
-						{
-							loader: 'postcss-loader',
-							options: {
-								// Necessary for external CSS imports to work
-								// https://github.com/facebookincubator/create-react-app/issues/2677
-								ident: 'postcss',
-								plugins: () => [
-									require('postcss-flexbugs-fixes'),
-									autoprefixer({
-										browsers: [
-											'>1%',
-											'last 4 versions',
-											'Firefox ESR',
-											'not ie < 9', // React doesn't support IE8 anyway
+					},
+					// Process JS with Babel.
+					{
+						test: /\.(js|jsx)$/,
+						include: paths.appSrc,
+						loader: 'babel-loader',
+						options: {
+							// This is a feature of `babel-loader` for webpack (not Babel itself).
+							// It enables caching results in ./node_modules/.cache/babel-loader/
+							// directory for faster rebuilds.
+							cacheDirectory: true,
+						},
+					},
+					// "postcss" loader applies autoprefixer to our CSS.
+					// "css" loader resolves paths in CSS and adds assets as dependencies.
+					// "style" loader turns CSS into JS modules that inject <style> tags.
+					// In production, we use a plugin to extract that CSS to a file, but
+					// in development "style" loader enables hot editing of CSS.
+					{
+						test: /\.scss$/,
+						use: ExtractTextPlugin.extract({ // @TODO review for webpack 4
+							fallback: 'style-loader',
+							use: [
+								{
+									loader: 'css-loader',
+									options: {
+										importLoaders: 2,
+										minimize: isProduction,
+										sourceMap: !isProduction,
+									},
+								},
+								{
+									loader: 'postcss-loader',
+									options: {
+										ident: 'postcss',
+										plugins: () => [
+											require('postcss-flexbugs-fixes'),
+											autoprefixer({
+												browsers: [
+													'>1%',
+													'last 4 versions',
+													'Firefox ESR',
+													'not ie < 9', // React doesn't support IE8 anyway
+												],
+												flexbox: 'no-2009',
+											}),
 										],
-										flexbox: 'no-2009',
-									}),
-								],
-								sourceMap: !isProduction,
-							},
+										sourceMap: !isProduction,
+									},
+								},
+								{
+									loader: 'sass-loader',
+									options: {
+										sourceMap: !isProduction,
+									},
+								},
+							],
+						}),
+					},
+					// "file" loader makes sure those assets get served by WebpackDevServer.
+					// When you `import` an asset, you get its (virtual) filename.
+					// In production, they would get copied to the `build` folder.
+					// This loader doesn't use a "test" so it will catch all modules
+					// that fall through the other loaders.
+					{
+						// Exclude `js` files to keep "css" loader working as it injects
+						// its runtime that would otherwise processed through "file" loader.
+						// Also exclude `html` and `json` extensions so they get processed
+						// by webpacks internal loaders.
+						exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+						loader: 'file-loader',
+						options: {
+							name: 'files/[name].[hash:8].[ext]',
 						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sourceMap: !isProduction,
-							},
-						},
-					],
-				}),
+					},
+				],
 			},
 		],
 	},
@@ -136,7 +155,7 @@ module.exports = {
 				}),
 			},
 		}),
-		new webpack.NamedModulesPlugin(),
+		!isProduction && new webpack.NamedModulesPlugin(),
 		isProduction && new webpack.optimize.UglifyJsPlugin(),
 		isProduction && new CompressionPlugin({
 			asset: '[path].gz[query]',
@@ -153,5 +172,6 @@ module.exports = {
 		fs: 'empty',
 		net: 'empty',
 		tls: 'empty',
+		child_process: 'empty',
 	},
 };
