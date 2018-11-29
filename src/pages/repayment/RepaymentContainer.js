33f24dced253451ca75ae429aa9f28d22e8c9e60
0xTatsu <nghia@autonomous.nyc>
2018-11-29 21:36:56 +0700

    update repayment fields

--- src/pages/repayment/RepaymentContainer.js
+++ src/pages/repayment/RepaymentContainer.js
@@ -13,17 +13,26 @@ export class RepaymentContainer extends Component {
 
   handleOnSubmit = () => {};
 
-  renderForm = formProps => {
-    return <RepaymentForm formProps={formProps} />;
+  buildInitialValues = ({ requestedLoan }) => {
+    const { appliedDate, loanTerm, amount, repaymentFrequency } = requestedLoan;
+    const nextDeadlineUnit = repaymentFrequency.value.replace('ly', 's').toLowerCase();
+    const dateFormat = 'DD MMMM, YYYY';
+    return {
+      ...requestedLoan,
+      paymentMethods: '',
+      appliedDate: moment(appliedDate).format(dateFormat),
+      toDate: moment(appliedDate)
+        .add(loanTerm.value, 'M')
+        .format(dateFormat),
+      nextDeadline: moment(appliedDate)
+        .add(1, nextDeadlineUnit)
+        .format(dateFormat),
+      principalAmount: amount,
+    };
   };
 
-  renderComponent = props => {
-    const initialValues = {
-      ...props.requestedLoan,
-      paymentMethods: '',
-      appliedDate: moment(props.requestedLoan.appliedDate).format('DD MMMM YYYY'),
-    };
-    const validationSchema = Yup.object({
+  buildValidationSchema = () => {
+    return Yup.object({
       paymentMethods: Yup.object()
         .required('Required')
         .nullable(),
@@ -35,10 +44,17 @@ export class RepaymentContainer extends Component {
           return !f ? true : f.size < 5 * 1024 * 1024;
         }),
     });
+  };
+
+  renderForm = formProps => {
+    return <RepaymentForm formProps={formProps} />;
+  };
+
+  renderComponent = props => {
     return (
       <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
+        initialValues={this.buildInitialValues(props)}
+        validationSchema={this.buildValidationSchema()}
         render={this.renderForm}
         onSubmit={(values, actions) => this.handleOnSubmit(values, actions, props)}
       />
